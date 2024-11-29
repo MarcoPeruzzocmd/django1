796e8cf0426f7db8dd6797b31fd4ec1d614314ac
marcoperuzzocmd <marcoantperuzzo@gmail.com>
2024-11-29 16:43:59 -0300

    postman e autenticação de usuario

--- config/settings.py
+++ config/settings.py
@@ -40,8 +40,18 @@ INSTALLED_APPS = [
     "rest_framework",
     "livraria",
     "corsheaders",
+    "rest_framework_simplejwt",
+
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissions",
+    ],  
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- livraria/views.py
+++ livraria/views.py
@@ -1,5 +1,4 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
 from livraria.models import Categoria, Editora, Autor, Livro
 from livraria.serializers import CategoriaSerializer, EditoraSerializer, AutorSerializer, LivroSerializer, LivroDetailSerializer, LivroListSerializer
 
